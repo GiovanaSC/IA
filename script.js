0c98ec7b2c78c474d9b8d5507b18e8cc15ac37d3
GiovanaSC <107865393+GiovanaSC@users.noreply.github.com>
2024-06-11 16:20:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector ('.caixa-perguntas');
+const caixaAlternativas = document.querySelector ('.caixa-alternativas');
+const caixaResultado = document.querySelector ('.caixa-resultedo');
+const caixaResultado = document.querySelector ('.texto-resultado');
+
+
+const perguntas= [
+        {
+            enunciado: "Qual o nome da praia mais famosa do Rio de Janeiro? ",
+            alternativa: [ "Praia de Ipanema",
+            "Praia de Copacabana"
+            ]
+    },
+        {
+            enunciado: "pergunta 2",
+            alternativa: [ "Alternativa 1",
+            "Alternativa 2"
+            ]
+    },
+        {
+            enunciado: "pergunta 3",
+            alternativa: [ "Alternativa 1",
+            "Alternativa 2"
+            ]
+    },
+        {
+            enunciado: "pergunta 4",
+            alternativa: [ "Alternativa 1",
+            "Alternativa 2"
+            ]
+    },
+        {
+            enunciado: "pergunta 5",
+            alternativa: [ "Alternativa 1",
+            "Alternativa 2"
+            ]
+    } // fecha objeto de perguntas
+]; //fecha lista de perguntas
+
+
